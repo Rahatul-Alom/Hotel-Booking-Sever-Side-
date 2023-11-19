d347b6a54f755443980d7b4e5130c2e5d8239732
Rahatul-Alom <muhammadrahatulalom@gmail.com>
2023-11-19 19:53:17 +0600

    added booking post api

--- index.js
+++ index.js
@@ -60,14 +60,13 @@ async function run() {
     // await client.connect();
 
     const roomsCollection = client.db('roomDB').collection('rooms')
+    const bookingCollection = client.db('roomDB').collection('bookings')
 
     // auth related api
     app.post('/jwt', async(req, res)=>{
       const user = req.body
       console.log('user for token', user)
-
       const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '1hr'} )
-
       res.cookie('token', token, {
         httpOnly: true,
         secure: true,
@@ -77,19 +76,50 @@ async function run() {
     })
 
 
-
-    // rooms api
+  // rooms api
     app.get('/rooms', async (req, res)=>{
-      const cusor = roomsCollection.find();
-      const result = await cusor.toArray();
+      const cursor = roomsCollection.find();
+      const result = await cursor.toArray();
       res.send(result)
     })
 
     app.get('/rooms/:id', async (req, res)=>{
       const id = req.params.id;
-      const query = {_id: new ObjectId(id)}
+      const query = {_id: new ObjectId(id)};
       const result = await roomsCollection.findOne(query)
       res.send(result)
+    })
+
+
+
+  // booking api
+
+    app.get('/bookings', async(req, res)=>{
+      console.log(req.query.email);
+      console.log('token owner info', req.user)
+      // if(req.user.email !== req.query.email){
+      //     return res.status(403).send({message: 'forbidden access'})
+      // }
+      let query = {};
+      if (req.query?.email) {
+          query = { email: req.query.email }
+      }
+      const result = await bookingCollection.find(query).toArray();
+      res.send(result);
+    })
+
+    // app.get('bookings/:id', async (req, res)=>{
+    //   const id = req.params.id;
+    //   const query ={_id: new ObjectId(id)}
+    //   const result = await bookingCollection.findOne(query)
+    //   res.send(result)
+    // })
+
+    app.post('/bookings', async(req, res)=>{
+        const booking = req.body;
+        console.log(booking);
+        const result = await bookingCollection.insertOne(booking)
+        res.send(result)
     })
 
 
